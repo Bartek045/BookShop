cf19a3a7d02c3029e8771ba0c665416ff97ded4e
Bartek Kowalczyk <barteknock@outlook.com>
2023-12-25 02:51:52 +0100

    Add new category

--- BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
+++ BookShop/obj/Debug/net8.0/BookShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23cd4a2a31c41d9f4f16dabe3ad19efb34a54a94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de266a0587801291f227f2d3e2fad3793c417fd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
